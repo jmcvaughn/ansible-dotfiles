8ae7f36ec37700843c55bd83a13b1862a81cba63
James Vaughn <v0rngit@gmail.com>
2017-07-09 18:48:08 +0100

    Set zshrc role to recreate .zshrc.d/, fix PAGER var
    The zshrc role now recreates the .zshrc.d/ directory, so that any files
    in this directory that shouldn't be present are deleted on a playbook
    run.
    
    The PAGER variable now uses nvim's '-R' switch to set read-only.

--- roles/zshrc/files/50-variables.zsh
+++ roles/zshrc/files/50-variables.zsh
@@ -3,7 +3,7 @@ export CDPATH="$HOME"
 export EDITOR="/usr/bin/nvim"
 export PATH="$PATH:$HOME/bin"
 # Pagers
-export PAGER="/usr/bin/col --no-backspaces | /usr/bin/nvim -c 'set readonly'"
+export PAGER="/usr/bin/col --no-backspaces | /usr/bin/nvim -R"
 export GIT_PAGER="/usr/bin/less"
 export MANPAGER="/usr/bin/nvim -c 'set filetype=man' -"
 export SYSTEMD_PAGER="/usr/bin/less"
