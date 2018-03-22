67f052e5118a79c30f6155b13ac09b14abbb8667
James Vaughn <v0rngit@gmail.com>
2018-03-22 11:56:39 +0000

    Add case insensitive matching of lowercase to Zsh

--- roles/zshrc/files/common/30-completions.zsh
+++ roles/zshrc/files/common/30-completions.zsh
@@ -1,5 +1,10 @@
 # Completion - see zshcompsys(1)
 autoload -Uz compinit
 compinit
+
+# Unconditional menu completion
 zstyle ':completion:*' menu select
+
+# Case insensitive matching for lowercase
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 
