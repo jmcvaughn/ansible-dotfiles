af2d04ab0ee403a6ffb1ada2e7eae97fd3ab5e39
James Vaughn <v0rngit@gmail.com>
2018-04-09 23:46:28 +0100

    Add GNU coreutils to (MAN)PATH in zshrc

--- roles/zshrc/files/mac/55-variables.zsh
+++ roles/zshrc/files/mac/55-variables.zsh
@@ -1,3 +1,7 @@
 # Disable Homebrew analytics
 export HOMEBREW_NO_ANALYTICS=1
 
+# Add GNU coreutils references
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
