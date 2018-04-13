84de88269aff6582981b508ffff566508228f999
James Vaughn <v0rngit@gmail.com>
2018-04-13 15:49:15 +0100

    Fix Zsh completion on Mac

--- roles/zshrc/files/common/30-completions_zstyle.zsh
+++ roles/zshrc/files/common/30-completions_zstyle.zsh
@@ -1,6 +1,4 @@
 # Completion - see zshcompsys(1)
-autoload -Uz compinit
-compinit
 
 # Unconditional menu completion
 zstyle ':completion:*' menu select
---
+++ roles/zshrc/files/mac/29-compinit.zsh
@@ -0,0 +1,9 @@
+# Completion - see zshcompsys(1)
+
+# Add zsh-completions to function path
+fpath=(/usr/local/share/zsh-completions $fpath)
+
+# compinit
+autoload -Uz compinit
+compinit -u
+
