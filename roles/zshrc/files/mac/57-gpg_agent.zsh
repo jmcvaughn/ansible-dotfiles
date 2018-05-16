f2abe34263812241ed162c3e4a1ac4c7b2587f6a
James Vaughn <v0rngit@gmail.com>
2018-05-16 12:18:17 +0100

    Use gpg-agent for SSH authentication on PCs

---
+++ roles/zshrc/files/mac/57-gpg_agent.zsh
@@ -0,0 +1,12 @@
+# Use gpg-agent as SSH agent
+# Taken from gpg-agent(1) - set SSH agent socket
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+
+# Start gpg-agent as a daemon if not already running
+pgrep -U $UID gpg-agent
+if [ "$?" -ne 0 ]; then
+    gpg-agent --daemon
+fi
+
